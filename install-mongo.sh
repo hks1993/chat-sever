b7ced846ddbd4b09f59f05a5e123870cd653b48b
hks_93 <hemanth@piktorlabs.com>
2020-10-12 14:42:01 +0530

    no message

--- install-mongo.sh
+++ install-mongo.sh
@@ -1,2 +1,8 @@
 curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add 
 sudo apt-key list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update
+sudo apt install mongodb-org
+sudo systemctl start mongod.service
+sudo systemctl status mongod
+sudo systemctl enable mongod
