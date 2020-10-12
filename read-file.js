c35556fc80ea6c8953d48e33031e45a5d5a0b01f
hks_93 <hemanth@piktorlabs.com>
2020-10-13 00:51:52 +0530

    no message

--- read-file.js
+++ read-file.js
@@ -1,33 +1,52 @@
 var fs = require("fs");
-fs.readFile("sample.txt", "utf8", function (err, data) {
-  // console.dir(data);
+
+const datevalidator = (date = "18/12/2018") => {
+  try {
+    const [day, month, year] = date.split("/");
+    if (+day < 32 && +month < 13 && +year > 2000) {
+      return true;
+    }
+  } catch (e) {
+    return false;
+  }
+  return false;
+};
+
+fs.readFile("./chat-files/smaple.txt", "utf8", function (err, data) {
   const lines = data.split("\r\n");
   console.log("lines length", lines.length);
+
   const chatRecord = {};
   let previousObject = {};
   lines.forEach((line) => {
     try {
       const [date, date_remaining] = line.split(", ");
-
       const [time, time_remaining] = date_remaining.split(" - ");
       const [senderRaw, message] = time_remaining.split(": ");
       let sender = "Neha";
-      if (senderRaw === "Hemanth") {
+      if ((senderRaw || "").includes("Hemanth")) {
         sender = "Hemanth";
       }
-      if (!chatRecord[date]) {
-        chatRecord[date] = [{ date, time, sender, message }];
+
+      if (date && date.length < 12 && datevalidator(date) && time && sender) {
+        if (!chatRecord[date]) {
+          chatRecord[date] = [{ date, time, sender, message }];
+        } else {
+          chatRecord[date].push({ date, time, sender, message });
+        }
+        previousObject = { date, time, sender, message };
       } else {
-        chatRecord[date].push({ date, time, sender, message });
+        throw `${message} has no date `;
       }
-      previousObject = { date, time, sender, message };
     } catch (err) {
-      //console.error(err);
       if (previousObject && line) {
-        chatRecord[previousObject.date] = { ...previousObject, message: line };
-        console.log("added", line, "to", previousObject.date);
+        chatRecord[previousObject.date].push({
+          ...previousObject,
+          message: line,
+        });
+        // console.log("added", line, "to", previousObject.date);
       }
     }
   });
-  fs.writeFileSync("chat-dummy-small.json", JSON.stringify(chatRecord));
+  fs.writeFileSync("chat.json", JSON.stringify(chatRecord));
 });
