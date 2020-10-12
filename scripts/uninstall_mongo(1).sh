27b08567fda152df6f4e6276632a1f087c444bd2
hks_93 <hemanth@piktorlabs.com>
2020-10-12 14:56:16 +0530

    no message

---
+++ scripts/uninstall_mongo(1).sh
@@ -0,0 +1,3 @@
+sudo apt-get purge mongodb mongodb-server mongodb-server-core mongodb-clients
+sudo apt-get purge mongodb-org
+sudo apt-get autoremove
---
+++ scripts/uninstall_mongo(3).sh
@@ -0,0 +1,2 @@
+sudo dpkg --remove --force-remove-reinstreq mongo-tools
+sudo dpkg --remove --force-remove-reinstreq mongodb-server-core
